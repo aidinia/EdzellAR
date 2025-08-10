94561a2d0ef7d2686930dd9866e6fd9426553e1c
Aida Sorgentini <33468417+aidinia@users.noreply.github.com>
2025-08-10 16:13:33 +0100

    First change

--- script.js
+++ script.js
@@ -1,8 +1,1 @@
-const startButton = document.getElementById('start-button');
-const container = document.getElementById('container');
-const scene = document.querySelector('a-scene');
 
-startButton.addEventListener('click', () => {
-  container.style.display = 'none';
-  scene.style.display = 'block';
-});
